e5ead63b51d88a22b4ef7e78971925482ac73c6b
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-02 12:39:09 +0900

    Create test.js

---
+++ docs/test.js
@@ -0,0 +1,22 @@
+const video = document.createElement('video');
+video.onloadedmetadata = evt => {
+    video.play();
+    setup(video);
+};
+video.src = 'sample.mp4';
+
+function setup(video) {
+    const renderer = new THREE.WebGLBufferRenderer({});
+    document.body.appendChild(renderer.domElmeent);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, this.ouputSize.aspectRatio, 0.1, 10000);
+    const geometry = new THREE.SphereBufferGeometry(100, 32, 32);
+    const texture = new THREE.VideoTexture(video);
+    const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+    const mesh = new THREE.Mesh(geometry, material);
+}
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
