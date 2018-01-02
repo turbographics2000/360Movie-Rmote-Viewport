ef901df3f5d0ceb693990ddb939f518f12ea5888
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-02 13:04:13 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -5,16 +5,21 @@ video.onloadedmetadata = evt => {
 };
 video.src = 'sample.mp4';
 
+let renderer = null;
+let scene = null;
+let camera = null;
+
 function setup(video) {
-    const renderer = new THREE.WebGLRenderer({});
+    renderer = new THREE.WebGLRenderer({});
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
     const geometry = new THREE.SphereBufferGeometry(100, 32, 32);
     const texture = new THREE.VideoTexture(video);
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSize });
     const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
     render();
 }
 
