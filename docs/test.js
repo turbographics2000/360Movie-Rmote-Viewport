f832d78051278aac877a85e4f57dc09fd3aa7589
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-02 14:17:27 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -20,6 +20,7 @@ window.onkeydown = evt => {
 function setup(video) {
     renderer = new THREE.WebGLRenderer({});
     renderer.setSize(width, height);
+    renderer.domElement.style.display = 'inherits';
     document.body.appendChild(renderer.domElement);
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
