52ae23f227c8c58e94cc29f69e30e06b8377d705
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-02 12:58:57 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -10,7 +10,7 @@ function setup(video) {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, this.ouputSize.aspectRatio, 0.1, 10000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
     const geometry = new THREE.SphereBufferGeometry(100, 32, 32);
     const texture = new THREE.VideoTexture(video);
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
