22936aa3289640c2afaef0adf57911ce345d092e
turbographics2000 <turbographics2000@yahoo.co.jp>
2018-01-02 13:50:13 +0900

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -7,16 +7,23 @@ video.src = 'sample.mp4';
 
 let renderer = null;
 let scene = null;
+let texture = null;
 let camera = null;
+let isStereo = false;
+window.onkeydown = evt => {
+    if(evt.code === 'KeyS') isStereo = !isStereo;
+};
 
 function setup(video) {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
     renderer = new THREE.WebGLRenderer({});
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(w, h);
     document.body.appendChild(renderer.domElement);
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+    camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 10000);
     const geometry = new THREE.SphereBufferGeometry(100, 32, 32);
-    const texture = new THREE.VideoTexture(video);
+    texture = new THREE.VideoTexture(video);
     texture.minFilter = THREE.LinearFilter;
     texture.magFilter = THREE.LinearFilter;
     texture.format = THREE.RGBFormat;
@@ -24,10 +31,28 @@ function setup(video) {
     const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
-    render();
 }
 
 function render() {
     requestAnimationFrame(render);
+    if (isStereo) {
+        // ステレオレンダリング
+        w /= 2;
+        renderer.setScissorTest(true);
+        stereoRender(0, 0, w, h, 0.5);
+        stereoRender(w, 0, w, h, -0.5);
+        renderer.setScissorTest(false);
+    } else {
+        // ノーマルレンダリング
+        renderer.setViewport(0, 0, w, h);
+        renderer.render(scene, camera);
+    }    
+}
+
+function stereoRender(left, top, width, height, repeatY) {
+    texture.repeat.y = repeatY;
+    renderer.setViewport(left, top, width, height);
+    renderer.setScissor(left, top, width, height);
     renderer.render(scene, camera);
 }
+
